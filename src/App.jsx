f45dedf1415ff99402b3f4f65233dd480bbe72b0
sajibkhandev <sajibkhan.mern@gmail.com>
2025-02-19 18:37:43 +0600

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navber from './layouts/Navber'
+import Banner from './layouts/Banner'
 
 
 const App = () => {
   return (
     <div>
       <Navber/>
+      <Banner/>
       
      
     </div>
