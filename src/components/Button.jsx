40e4ec0bf419f29faaa1747dbf549ecfbc2acb46
sajibkhandev <sajibkhan.mern@gmail.com>
2025-03-13 15:31:32 +0600

    resposive added 2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({text,className}) => {
   return (
-    <button className={`relative after:absolute after:top-[5px] after:left-[5px] after:w-[95%] after:h-[87%] after:content-[""] after:border-white after:border py-6 px-11 bg-secondary text-lg text-white font-medium font-jost ${className}`}>{text}</button>
+    <button className={`relative after:absolute after:top-[5px] after:left-[5px] after:w-[92%] md:after:w-[95%] after:h-[82%]  md:after:h-[87%] after:content-[""] after:border-white after:border py-3 px-6 md:py-6 md:px-11 bg-secondary text-lg text-white font-medium font-jost ${className}`}>{text}</button>
   )
 }
 
--- src/layouts/ServiceLayout.jsx
+++ src/layouts/ServiceLayout.jsx
@@ -9,13 +9,13 @@ import Heading from '../components/Heading'
 
 const ServiceLayout = () => {
   return (
-    <section className='pt-[220px] pb-[120px]'>
+    <section className='pt-[100px] md:pt-[220px] pb-[60px] md:pb-[120px]'>
         <Container>
           <SubHeading text="MY SERVICES"/>
-            <Heading text='Provide Wide Range of Digital Services'/>
+            <Heading className='w-auto md:w-[522px] text-4xl md:text-[50px] md:leading-[60px]' text='Provide Wide Range of Digital Services'/>
         
 
-            <Flex className='justify-between pt-10'>
+            <Flex className='flex-wrap justify-center md:justify-between pt-10 px-3 sm:px-3 md:px-0'>
             <ServiceCard src={ServiceImage1} title="Ui/Ux Design" text="Dolor repellendus tempo ribus aue quibusdam offi ciis debitis rerum na aibus minima veniam."/>
             <ServiceCard src={ServiceImage2} title="Mobile App Design" text="Dolor repellendus tempo ribus aue quibusdam offi ciis debitis rerum na aibus minima veniam."/>
             <ServiceCard src={ServiceImage1} title="Web Design" text="Dolor repellendus tempo ribus aue quibusdam offi ciis debitis rerum na aibus minima veniam."/>
